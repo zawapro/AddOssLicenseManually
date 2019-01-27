d2a07d9e6710d8329652e0454d20537085273ec5
zawatan <zawatan@zawapro.com>
2019-01-27 10:37:33 +0900

    OssLicensesライブラリの追加と表示

--- app/src/main/java/com/zawapro/addosslicensemanually/MainActivity.kt
+++ app/src/main/java/com/zawapro/addosslicensemanually/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.zawapro.addosslicensemanually
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // OssLicensesMenuActivityにインテントを投げる
+        findViewById<Button>(R.id.button).setOnClickListener{
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        }
     }
 }
